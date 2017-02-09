9f444e36946125b375cd279fb66d73b1e6664d98
Richard W. Eitz <richard.eitz@gravidade.org>
2017-02-09 09:00:04 -0200

    Update seeds.sql

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,3 +1,3 @@
-INSERT INTO `user` (name, devoured, date) VALUES ('Bob', true, current_timestamp);
-INSERT INTO `user` (name, devoured, date) VALUES ('Billy', false, current_timestamp);
-INSERT INTO `user` (name, devoured, date) VALUES ('Jie', false, current_timestamp);
+INSERT INTO `user` (name, active, date) VALUES ('Bob', true, current_timestamp);
+INSERT INTO `user` (name, active, date) VALUES ('Billy', false, current_timestamp);
+INSERT INTO `user` (name, active, date) VALUES ('Jie', false, current_timestamp);
